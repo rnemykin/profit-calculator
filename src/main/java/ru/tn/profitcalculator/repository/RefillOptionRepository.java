2edcee0e4a1d897b671e134295a09992a1625e5e
RNemykin <nemykinroman@gmail.com>
2017-07-22 01:09:26 +0300

    option repositories

---
+++ src/main/java/ru/tn/profitcalculator/repository/RefillOptionRepository.java
@@ -0,0 +1,9 @@
+package ru.tn.profitcalculator.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import ru.tn.profitcalculator.model.RefillOption;
+
+@RepositoryRestResource
+public interface RefillOptionRepository extends JpaRepository<RefillOption, Long>{
+}
