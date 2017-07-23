4113cdf783723cbc5275f4e2d0fbdce7a2f4ddc1
RNemykin <nemykinroman@gmail.com>
2017-07-24 01:15:55 +0300

    tests

--- src/test/java/ru/tn/profitcalculator/ProfitCalculatorApplicationTests.java
+++ src/test/java/ru/tn/profitcalculator/ProfitCalculatorApplicationTests.java
@@ -6,7 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import ru.tn.profitcalculator.model.Product;
+import ru.tn.profitcalculator.model.enums.ProductTypeEnum;
+import ru.tn.profitcalculator.repository.DepositRepository;
+import ru.tn.profitcalculator.repository.SavingAccountRepository;
 import ru.tn.profitcalculator.service.ProductService;
+import ru.tn.profitcalculator.service.calculator.CalculateRequest;
+import ru.tn.profitcalculator.service.calculator.Calculator;
+import ru.tn.profitcalculator.service.calculator.CalculatorFactory;
 
 import java.math.BigDecimal;
 import java.util.List;
@@ -18,10 +24,34 @@ public class ProfitCalculatorApplicationTests {
 	@Autowired
 	private ProductService productService;
 
+	@Autowired
+	CalculatorFactory calculatorFactory;
+
+	@Autowired
+	DepositRepository depositRepository;
+
+	@Autowired
+	SavingAccountRepository savingAccountRepository;
 
 	@Test
-	public void findAll() {
+	public void findProducts() {
 		List<Product> products = productService.searchProducts(5, BigDecimal.ONE, BigDecimal.TEN);
 		System.out.println(products);
+	}
+
+	@Test
+	public void savingAccountCalculate() {
+		Calculator depositCalculator = calculatorFactory.get(ProductTypeEnum.SAVING_ACCOUNT);
+		savingAccountRepository.findAll().forEach(d -> System.out.println(
+				depositCalculator.calculate(
+						CalculateRequest.builder()
+								.product(d)
+								.daysCount(365)
+								.monthRefillSum(BigDecimal.valueOf(20000))
+								.initSum(BigDecimal.valueOf(200000))
+								.build()
+				)
+		));
+
 	}
 }
