adf0b11bdb5b79014ba913b05551db4a381612b6
Ilya Lysenko <lysenko.ilya@gmail.com>
2017-07-24 00:17:26 +0300

    javadocs for enums

--- src/main/java/ru/tn/profitcalculator/model/enums/RefillOptionEventTypeEnum.java
+++ src/main/java/ru/tn/profitcalculator/model/enums/RefillOptionEventTypeEnum.java
@@ -1,7 +1,22 @@
 package ru.tn.profitcalculator.model.enums;
 
+/**
+ * Тип события
+ */
 public enum RefillOptionEventTypeEnum {
+
+    /**
+     * просто фиксированная дата
+     */
     FIXED_DATE,
+
+    /**
+     * любое поступление средств на счет (в т.ч. ЗП)
+     */
     TRANSFER,
+
+    /**
+     * любая покупка по карте
+     */
     CARD_PURCHASES
 }
--- src/main/java/ru/tn/profitcalculator/model/enums/RefillOptionSumTypeEnum.java
+++ src/main/java/ru/tn/profitcalculator/model/enums/RefillOptionSumTypeEnum.java
@@ -1,8 +1,27 @@
 package ru.tn.profitcalculator.model.enums;
 
+/**
+ * Сумма автопополнения
+ */
 public enum RefillOptionSumTypeEnum {
+
+    /**
+     * фиксированная сумма
+     */
     FIXED_SUM,
+
+    /**
+     * ??? TODO в презе не было
+     */
     FIXED_PERCENTAGE,
+
+    /**
+     * •%% от операций (поступившей/ потраченной суммы)
+     */
     IN_OUT_SUM_PERCENTAGE,
+
+    /**
+     * сдача с покупки (разница между округленной до тысяч суммой покупки и фактической суммой покупки)
+     */
     PAYMENT_CHANGE
 }
