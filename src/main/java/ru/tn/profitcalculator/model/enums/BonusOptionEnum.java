afcc4e82c0fd1909b362c6f57326609c6fcbe242
Ilya Lysenko <lysenko.ilya@gmail.com>
2017-07-25 00:47:50 +0300

    javadocs

--- src/main/java/ru/tn/profitcalculator/model/enums/BonusOptionEnum.java
+++ src/main/java/ru/tn/profitcalculator/model/enums/BonusOptionEnum.java
@@ -1,10 +1,36 @@
 package ru.tn.profitcalculator.model.enums;
 
+/**
+ * Бонусные опции
+ */
 public enum BonusOptionEnum {
+
+    /**
+     * Авто
+     */
     AUTO,
+
+    /**
+     * Кафе
+     */
     CAFE,
+
+    /**
+     * Путешествия
+     */
     TRAVEL,
+
+    /**
+     * Коллекция
+     */
     COLLECTION,
+    /**
+     * Сберегатель
+     */
     SAVING,
+
+    /**
+     * Cash back
+     */
     CASH_BACK
 }
