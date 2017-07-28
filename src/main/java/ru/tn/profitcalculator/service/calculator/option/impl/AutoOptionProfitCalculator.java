fb96ac4de30647c607f4894963076b4e82037bba
Ilya Lysenko <lysenko.ilya@gmail.com>
2017-07-29 02:22:52 +0300

    updated option profit calculators

--- src/main/java/ru/tn/profitcalculator/service/calculator/option/impl/AutoOptionProfitCalculator.java
+++ src/main/java/ru/tn/profitcalculator/service/calculator/option/impl/AutoOptionProfitCalculator.java
@@ -2,6 +2,7 @@ package ru.tn.profitcalculator.service.calculator.option.impl;
 
 import org.springframework.stereotype.Service;
 import ru.tn.profitcalculator.model.enums.BonusOptionEnum;
+import ru.tn.profitcalculator.model.enums.PosCategoryEnum;
 
 /**
  * Калькулятор ставки и суммы кэшбека по опции Авто
@@ -12,5 +13,10 @@ public class AutoOptionProfitCalculator extends CashbackOptionProfitCalculator {
     @Override
     public BonusOptionEnum getOption() {
         return BonusOptionEnum.AUTO;
+    }
+
+    @Override
+    PosCategoryEnum getOptionCategory() {
+        return PosCategoryEnum.AUTO;
     }
 }
